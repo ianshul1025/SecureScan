c2a32ed3a1376ebd98b6174f51eab274dc3e6f1f
ianshul1025 <saritakumari09814@gmail.com>
2026-07-22 23:53:59 +0530

    Initial commit

---
+++ src/main/java/com/anshul/securescan/SecureScanApplication.java
@@ -0,0 +1,13 @@
+package com.anshul.securescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureScanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anshul/securescan/SecureScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.securescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
